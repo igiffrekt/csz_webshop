a4a848ae24ac0ae40a6a0a92b776ef03165fb035
stickerey <stickerey@gmail.com>
2026-01-20 09:30:07 +0100

    fix(02-01): remove products relation until Product type exists
    - Strapi 5 fails on forward references to non-existent content types
    - Products relation will be added in 02-02 when Product is created
    - Generated types updated

--- apps/cms/types/generated/components.d.ts
+++ apps/cms/types/generated/components.d.ts
@@ -1,3 +1,35 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface ProductCertification extends Struct.ComponentSchema {
+  collectionName: 'components_product_certifications';
+  info: {
+    displayName: 'Certification';
+  };
+  attributes: {
+    certificate: Schema.Attribute.Media<'files'>;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    standard: Schema.Attribute.String;
+    validUntil: Schema.Attribute.Date;
+  };
+}
+
+export interface ProductSpecification extends Struct.ComponentSchema {
+  collectionName: 'components_product_specifications';
+  info: {
+    displayName: 'Specification';
+  };
+  attributes: {
+    key: Schema.Attribute.String & Schema.Attribute.Required;
+    unit: Schema.Attribute.String;
+    value: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'product.certification': ProductCertification;
+      'product.specification': ProductSpecification;
+    }
+  }
+}
--- apps/cms/types/generated/contentTypes.d.ts
+++ apps/cms/types/generated/contentTypes.d.ts
@@ -430,6 +430,39 @@ export interface AdminUser extends Struct.CollectionTypeSchema {
   };
 }
 
+export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
+  collectionName: 'categories';
+  info: {
+    displayName: 'Category';
+    pluralName: 'categories';
+    singularName: 'category';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    children: Schema.Attribute.Relation<'oneToMany', 'api::category.category'>;
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    description: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::category.category'
+    > &
+      Schema.Attribute.Private;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    parent: Schema.Attribute.Relation<'manyToOne', 'api::category.category'>;
+    publishedAt: Schema.Attribute.DateTime;
+    slug: Schema.Attribute.UID<'name'> & Schema.Attribute.Required;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -940,6 +973,7 @@ declare module '@strapi/strapi' {
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
       'admin::user': AdminUser;
+      'api::category.category': ApiCategoryCategory;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
